5b2d04db79026f7acb3ff85599d209e25ef99526
h-sea-f <Hehaifeng2019>
2019-07-17 17:05:29 +0800

    add CompanyController CRUD

--- src/main/java/com/tw/apistackbase/controller/CompanyController.java
+++ src/main/java/com/tw/apistackbase/controller/CompanyController.java
@@ -3,9 +3,9 @@ package com.tw.apistackbase.controller;
 import com.tw.apistackbase.model.Company;
 import com.tw.apistackbase.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class CompanyController {
@@ -14,6 +14,26 @@ public class CompanyController {
 
     @PostMapping("/companies")
     public Company create(@RequestBody Company company) {
+        return companyRepository.save(company);
+    }
+
+    @GetMapping("/companies")
+    public List<Company> findAll() {
+        return companyRepository.findAll();
+    }
+
+    @GetMapping("/companies/{id}")
+    public Company findById(@PathVariable int id) {
+        return companyRepository.findById(id).get();
+    }
+
+    @DeleteMapping("/companies/{id}")
+    public void delete(@PathVariable int id) {
+        companyRepository.deleteById(id);
+    }
+
+    @PutMapping("/companies")
+    public Company update(@RequestBody Company company) {
         return companyRepository.save(company);
     }
 
